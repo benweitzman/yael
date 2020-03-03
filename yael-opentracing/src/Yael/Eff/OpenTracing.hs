58f4aa4da0581a09e4dfe4359c03baa8816a90b1
Ben Weitzman <benweitzman@gmail.com>
2020-03-03 17:00:31 -0500

    rework yael-opentracing to not use MonadReader or Traceable class

--- yael-opentracing/src/Yael/Eff/OpenTracing.hs
+++ yael-opentracing/src/Yael/Eff/OpenTracing.hs
@@ -1,81 +1,87 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 module Yael.Eff.OpenTracing
-  ( HigherOrder(..)
-  , Tracer(..)
-  , defaultTracer
-  , Traceable(..)
-  , Tracing(..)
-  , traceWrapL
+  ( Tracing(..)
+  , spanning
+  , modifyActive
   , mkSpanOpts
   , defaultSpanOpts
+  , _defaultSpanOpts
+  , defaultTrace
+  , otTracing
   ) where
 
-import Data.Monoid (Last(..))
-import Data.Maybe
-import Control.Lens (setting, Setter')
-import Data.Generic.HKD
-import GHC.Generics
-import GHC.Exts
-import Data.Functor.Const
-import           OpenTracing             hiding (HasTracer, Tracer, traced_, tracer)
-import qualified Data.Text as T
-import Data.Functor.Product
-import Data.Barbie.Constraints
-import Control.Monad.Reader
-import GHC.TypeLits
+import Control.Exception.Safe
+import Control.Monad
+import Control.Monad.IO.Class
 import Data.Proxy
+import qualified Data.Text as T
+import GHC.Exts
+import GHC.Generics
+import GHC.TypeLits
+import OpenTracing (ActiveSpan, Span, SpanOpts, spanOpts, childOf)
+import qualified OpenTracing             as OT
+import qualified OpenTracing.Standard    as OT
+import OpenTracing.Tracer      (traced_)
 import Yael.Eff
-import Yael.Eff.Data
+import Yael.Eff.Reader
 
-defaultTracer
-  :: forall f m
-   . ( ConstraintsB (HKD (f m))
-     , AllB (Split m) (HKD (f m))
-     , Label (f m)
-     , Generic (f m)
-     )
-  => HKD (f m) (Tracer m)
-defaultTracer = bmap mkTracer (baddDicts label)
-  where
-    mkTracer :: forall a . Product (Dict (Split m)) (Const String) a -> Tracer m a
-    mkTracer (Pair Dict (Const name)) = Tracer
-      { traceWrap = \tracing method -> singleUse tracing name method
-      }
+data Tracing m = Tracing
+  { _spanning :: forall a . SpanOpts -> m a -> m a
+  , _modifyActive :: (Span -> Span) -> m ()
+  , _mkSpanOpts :: T.Text -> (SpanOpts -> SpanOpts) -> m SpanOpts
+  }
+
+spanning :: (HasEff Tracing f m) => SpanOpts -> EffT f m a -> EffT f m a
+spanning opts block = withEffT' $ \lower Tracing{_spanning} ->
+  _spanning opts (lower block)
+
+modifyActive :: (Span -> Span) -> () :+ '[Tracing]
+modifyActive f = withEffT $ \Tracing{_modifyActive} -> _modifyActive f
+
+mkSpanOpts :: T.Text -> (SpanOpts -> SpanOpts) -> SpanOpts :+ '[Tracing]
+mkSpanOpts name f = withEffT $ \Tracing{_mkSpanOpts} ->
+  _mkSpanOpts name f
+
+otTracing
+  :: (MonadIO m, MonadMask m)
+  => Reader ParentSpan m
+  -> OT.Tracer
+  -> Tracing m
+otTracing Reader{_ask,_local} tracer' = Tracing
+  { _spanning = \opts block -> traced_ tracer' opts $ \activeSpan ->
+      _local (const $ Just activeSpan) block
+
+  , _modifyActive = \f -> do
+      mActive <- _ask
+      void . forM mActive $ \active ->
+        liftIO $ OT.modifyActiveSpan active f
+
+  , _mkSpanOpts = \name f -> do
+      parent <- _ask
+      return . f . spanOpts name $ maybe mempty childOf parent
+  }
 
 type ParentSpan = Maybe ActiveSpan
 
-newtype Tracer m a = Tracer
-  { traceWrap :: MonadReader ParentSpan m => Tracing m -> a -> a
-  }
+_defaultSpanOpts :: Tracing m -> T.Text -> m SpanOpts
+_defaultSpanOpts Tracing{_mkSpanOpts} n = _mkSpanOpts n id
 
-traceWrapL :: Setter' (Tracer m a) (Tracing m -> a -> a)
-traceWrapL = setting $ \m x -> x {traceWrap = m (traceWrap x)}
-
-mkSpanOpts
-  :: MonadReader ParentSpan m
-  => T.Text
-  -> (SpanOpts -> SpanOpts)
-  -> m SpanOpts
-mkSpanOpts t f = do
-  parent <- ask
-  return . f . spanOpts t $ maybe mempty childOf parent
-
-defaultSpanOpts t = mkSpanOpts t id
+defaultSpanOpts :: T.Text -> SpanOpts :+ '[Tracing]
+defaultSpanOpts name = mkSpanOpts name id
 
 class Split m a where
   singleUse
-    :: (MonadReader ParentSpan m)
+    :: Monad m
     => Tracing m
     -> String
     -> a
     -> a
 
 instance {-# OVERLAPPING #-} Split m (HigherOrder n m) where
-  singleUse Tracing{spanning} name (HigherOrder f) = HigherOrder $ \x -> do
-    opts <- spanOpts (T.pack name) . maybe mempty childOf <$> ask
-    spanning opts $ \activeSpan ->
-      local (const $ Just activeSpan) $ f x
+  singleUse t@Tracing{_spanning} name (HigherOrder f) = HigherOrder $ \x -> do
+    opts <- _defaultSpanOpts t (T.pack name)
+    _spanning opts $ f x
 
 instance {-# OVERLAPPING #-} (Split m b) => Split m (a -> b) where
   singleUse t n ab x = singleUse t n (ab x)
@@ -85,56 +91,31 @@ type family CheckFun m :: Constraint where
   CheckFun x = ()
 
 instance {-# INCOHERENT #-} (CheckFun m) => Split m (m x) where
-  singleUse Tracing{spanning} name x = do
-    opts <- spanOpts (T.pack name) . maybe mempty childOf <$> ask
-    spanning opts $ \activeSpan ->
-      local (const $ Just activeSpan) x
+  singleUse t@Tracing{_spanning} name x = do
+    opts <- _defaultSpanOpts t (T.pack name)
+    _spanning opts $ x
 
-data Tracing m = Tracing
-  { spanning :: forall a . SpanOpts -> (ActiveSpan -> m a) -> m a
-  , modifyActive :: (Span -> Span) -> m ()
-  , parentSpan :: ParentSpan
-  }
+defaultTrace
+  :: forall m f
+   . ( GTraceable m (Rep (f m))
+     , Generic (f m)
+     , CheckFun m
+     , Monad m
+     )
+  => Tracing m -> f m -> f m
+defaultTrace f fm = to $ gTracing f (from fm)
 
-class Traceable f where
-  tracing :: (TracerC f m, MonadReader ParentSpan m, CheckFun m) => Tracing m -> f m -> f m
+class GTraceable m f where
+  gTracing :: (Monad m) => Tracing m -> f x -> f x
 
-  default tracing
-    :: forall m
-     . ( GTraceable m (Rep (HKD (f m) (Tracer m))) (Rep (f m))
-       , Generic (f m)
-       , Generic (HKD (f m) (Tracer m))
-       , TracerC f m
-       , MonadReader ParentSpan m
-       , CheckFun m
-       )
-    => Tracing m -> f m -> f m
-  tracing f fm = to $ gTracing f (from $ tracer @f f) (from fm)
+instance (Split m v, KnownSymbol n) =>
+  GTraceable m (S1 ('MetaSel ('Just n) u s l) (K1 r v)) where
 
-  type TracerC f :: (* -> *) -> Constraint
-  type TracerC f = Monad
+  gTracing tracer (M1 (K1 x)) = M1 . K1 $
+    singleUse tracer (symbolVal $ Proxy @n) x
 
-  tracer :: (TracerC f m, MonadReader ParentSpan m, CheckFun m) => Tracing m -> HKD (f m) (Tracer m)
+instance GTraceable m f => GTraceable m (M1 k x f) where
+  gTracing tracer (M1 x) = M1 $ gTracing tracer x
 
-  default tracer
-    :: ( ConstraintsB (HKD (f m))
-       , AllB (Split m) (HKD (f m))
-       , Label (f m)
-       , Generic (f m)
-       , CheckFun m
-       )
-    => Tracing m -> HKD (f m) (Tracer m)
-  tracer _tr = defaultTracer
-
-class GTraceable m f g | f -> m where
-  gTracing :: MonadReader ParentSpan m => Tracing m -> f x -> g x -> g x
-
-instance (Monad m) => GTraceable m (K1 r (Tracer m v)) (K1 r v) where
-  gTracing tracer (K1 (Tracer f)) (K1 x) = K1 $ do
-    f tracer x
-
-instance GTraceable m t f => GTraceable m (M1 k x t) (M1 k x f) where
-  gTracing tracer  (M1 x) (M1 y) = M1 $ gTracing tracer x y
-
-instance (GTraceable m tl fl, GTraceable m tr fr) => GTraceable m (tl :*: tr) (fl :*: fr) where
-  gTracing tracer  (tl :*: tr) (fl :*: fr) = gTracing tracer tl fl :*: gTracing tracer tr fr
+instance (GTraceable m f, GTraceable m g) => GTraceable m (f :*: g) where
+  gTracing tracer (f :*: g) = gTracing tracer f :*: gTracing tracer g
